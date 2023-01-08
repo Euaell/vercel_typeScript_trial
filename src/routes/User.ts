934027b3f40457e25c5168b4284e5355297ecfb8
Euael Eshete <euaelmeko@gmail.com>
2023-01-08 14:34:54 +0300

    remove model

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -6,9 +6,5 @@ const { UserController } = controllers
 const router = Router()
 
 router.get('/', UserController.getUsers)
-router.post('/register', UserController.register)
-router.get('/:id', UserController.getUser)
-router.put('/:id', UserController.updateUser)
-router.delete('/:id', UserController.deleteUser)
 
 export default router
