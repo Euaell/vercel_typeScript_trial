2eb0bc0cb83609d4bbf927e522ba363fd0475e54
Euael Eshete <euaelmeko@gmail.com>
2023-01-08 15:02:46 +0300

    mongo connection with user model

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 
+import User from "../models/User"
+
 
 export default class UserController {
     
@@ -8,7 +10,20 @@ export default class UserController {
             res.status(200).json({
                 success: true,
                 message: "Users fetched successfully!"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
 
+    public static async createUser(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { name, email, password } = req.body
+            const user = await User.create({ name, email, password })
+            res.status(201).json({
+                success: true,
+                message: "User created successfully!",
+                data: user
             })
         } catch (error) {
             next(error)
