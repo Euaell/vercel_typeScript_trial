533056cc9abf36af9d953dc0c1f2c667aa532148
Euael Eshete <euaelmeko@gmail.com>
2023-01-08 13:31:12 +0300

    trying

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", route.HomeRoute)
-app.get("/api/v1/", route.HomeRoute)
+app.use("/", route.HomeRoute)
+app.use("/api/v1/", route.HomeRoute)
 
 app.use(ErrorHandler)
 
